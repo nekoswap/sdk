3525473c0bf88b9e228e8be106b5e30de1128b66
nekoswap <admin@nekoswap.org>
2021-11-01 11:25:51 +0800

    init comit

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import JSBI from 'jsbi'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const THREE = JSBI.BigInt(3)
+export const _9975 = JSBI.BigInt(9975)
+export const _10000 = JSBI.BigInt(10000)
