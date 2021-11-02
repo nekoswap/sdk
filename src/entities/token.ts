59e6c3bfdc20dcc75a4c5ae366ec93e7205340a0
Lint Action <lint-action@samuelmeuli.com>
2021-11-02 06:10:10 +0000

    Fix code style issues with Prettier

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -71,13 +71,7 @@ export const WETH: Record<number, Token> = {
     'WETH',
     'Wrapped Ether'
   ),
-  [ChainId.BSC]: new Token(
-    ChainId.BSC,
-    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    18,
-    'WBNB',
-    'Wrapped Ether'
-  ),
+  [ChainId.BSC]: new Token(ChainId.BSC, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped Ether'),
   [ChainId.OEC_TEST]: new Token(
     ChainId.OEC_TEST,
     '0x70c1c53E991F31981d592C2d865383AC0d212225',
@@ -85,13 +79,7 @@ export const WETH: Record<number, Token> = {
     'WOKT',
     'Wrapped Ether'
   ),
-  [ChainId.OEC]: new Token(
-    ChainId.OEC,
-    '0x8F8526dbfd6E38E3D8307702cA8469Bae6C56C15',
-    18,
-    'WOKT',
-    'Wrapped Ether'
-  ),
+  [ChainId.OEC]: new Token(ChainId.OEC, '0x8F8526dbfd6E38E3D8307702cA8469Bae6C56C15', 18, 'WOKT', 'Wrapped Ether'),
   [ChainId.BSC_TEST]: new Token(
     ChainId.BSC_TEST,
     '0x094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F',
@@ -99,13 +87,7 @@ export const WETH: Record<number, Token> = {
     'WBNB',
     'Wrapped Ether'
   ),
-  [ChainId.HECO]: new Token(
-    ChainId.HECO,
-    '0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F',
-    18,
-    'WHT',
-    'Wrapped Ether'
-  ),
+  [ChainId.HECO]: new Token(ChainId.HECO, '0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F', 18, 'WHT', 'Wrapped Ether'),
   [ChainId.HECO_TEST]: new Token(
     ChainId.HECO_TEST,
     '0x7aF326B6351C8A9b8fb8CD205CBe11d4Ac5FA836',
@@ -119,5 +101,5 @@ export const WETH: Record<number, Token> = {
     18,
     'WMOVR',
     'Wrapped Ether'
-  ),
+  )
 }
